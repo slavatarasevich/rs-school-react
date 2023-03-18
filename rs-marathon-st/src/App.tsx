a531309fad667c291482081dd7cfef2fe631360e
slavatarasevich <blackgrif@gmail.com>
2023-03-18 11:42:12 +0300

    Finish refactoring

--- rs-marathon-st/src/App.tsx
+++ rs-marathon-st/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import NotFound from './pages/404';
 import About from './pages/About';
 import Header from './components/Header';
+
+const NotFound = React.lazy(() => import('./pages/404'));
+
+///ToDo home about lazy!
 
 class App extends React.Component {
   render() {
