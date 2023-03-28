21282f154a6a76331df3b0569ea39bc937da3013
slavatarasevich <blackgrif@gmail.com>
2023-03-28 14:15:07 +0300

    refactor started

--- rs-marathon-st/src/pages/Home/Home.tsx
+++ rs-marathon-st/src/pages/Home/Home.tsx
@@ -1,49 +1,31 @@
 /* eslint-disable react/destructuring-assignment */
-import React from 'react';
+import React, { useState } from 'react';
 import CardsList from '../../components/CardsList';
 import data from '../../mock/fakeData.json';
 import './style.scss';
 
 type Data = { id: number; img: string; title: string; price: number }[];
 
-class Home extends React.Component<unknown, { value: string; dataList: Data }> {
+function Home() {
   // eslint-disable-next-line @typescript-eslint/ban-types
-  constructor(props: {} | Readonly<unknown>) {
-    super(props);
-    this.state = {
-      value: '',
-      dataList: [],
-    };
-  }
 
-  componentDidMount() {
-    const keyInput = localStorage.getItem('keyInput');
-    this.setState({ value: keyInput ?? '' });
-    this.setState({ dataList: data });
-  }
+  // componentDidMount() {
+  //   const keyInput = localStorage.getItem('keyInput');
+  //   this.setState({ value: keyInput ?? '' });
+  //   this.setState({ dataList: data });
+  // }
 
-  componentWillUnmount() {
-    localStorage.setItem('keyInput', this.state.value);
-  }
+  // componentWillUnmount() {
+  //   localStorage.setItem('keyInput', this.state.value);
+  // }
 
-  inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ value: e.target.value });
-  };
-
-  render() {
-    return (
-      <div className="home-container">
-        <h1>Home page</h1>
-        <input
-          type="text"
-          onChange={this.inputHandler}
-          value={this.state.value}
-          placeholder="Search"
-        />
-        <CardsList data={this.state.dataList} />
-      </div>
-    );
-  }
+  return (
+    <div className="home-container">
+      <h1>Home page</h1>
+      <input type="text" onChange={inputHandler} value={} placeholder="Search" />
+      <CardsList data={this.state.dataList} />
+    </div>
+  );
 }
 
 export default Home;
