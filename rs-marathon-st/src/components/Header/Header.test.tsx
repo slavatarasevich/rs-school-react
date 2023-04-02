058b9af7f4634999f8765810cd48a8da5c60d68c
slavatarasevich <blackgrif@gmail.com>
2023-04-02 16:28:02 +0300

    fix: fixing some stuff

--- rs-marathon-st/src/components/Header/Header.test.tsx
+++ rs-marathon-st/src/components/Header/Header.test.tsx
@@ -1,31 +1,17 @@
-import { describe, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Header from './index';
 
 describe('Header', () => {
-  it('renders Home and About links', () => {
-    render(
+  test('renders header with nav links', () => {
+    const { getByText } = render(
       <MemoryRouter>
-        {' '}
-        <Header />{' '}
+        <Header />
       </MemoryRouter>
     );
-    const homeLink = screen.getByRole('link', { name: 'Home' });
-    const aboutLink = screen.getByRole('link', { name: 'About us' });
-    expect(homeLink).toBeInTheDocument();
-    expect(aboutLink).toBeInTheDocument();
-  });
-  it('sets active class on active link', () => {
-    render(
-      <MemoryRouter initialEntries={['/']}>
-        {' '}
-        <Header />{' '}
-      </MemoryRouter>
-    );
-    const homeLink = screen.getByRole('link', { name: 'Home' });
-    const aboutLink = screen.getByRole('link', { name: 'About us' });
-    expect(homeLink).toHaveClass('link-active');
-    expect(aboutLink).not.toHaveClass('link-active');
+
+    expect(getByText('Home')).toBeInTheDocument();
+    expect(getByText('Form')).toBeInTheDocument();
+    expect(getByText('About us')).toBeInTheDocument();
   });
 });
